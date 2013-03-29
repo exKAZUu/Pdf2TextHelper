44e3a7f34e87808f1074fe16ac8b58c7cb84d276
Kazunori Sakamoto <exkazuu@gmail.com>
2013-03-29 14:31:36 +0900

    Add initial files.

---
+++ Pdf2TextHelper/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Pdf2TextForAntConc
+{
+	class Program
+	{
+		static void Main(string[] args) {
+			//args = new[] { @"C:\Users\exKAZUu\Dropbox\Private\Tools\English\pdfs" };
+			args = new[] { @"C:\Users\exKAZUu\Dropbox\Private\TeX\EclipseWorkspace\GAS2013" };
+			var filePaths = args.SelectMany(a => Directory.EnumerateFiles(a, "*.pdf"));
+			foreach (var filePath in filePaths) {
+				var info = new ProcessStartInfo {
+						FileName = "pdftotext.exe",
+						Arguments = "-enc UTF-8 \"" + filePath + "\"",
+						CreateNoWindow = true,
+						UseShellExecute = false,
+				};
+				var proc = Process.Start(info);
+				var textPath = Path.ChangeExtension(filePath, ".txt");
+				proc.WaitForExit();
+				var text = File.ReadAllText(textPath, Encoding.UTF8);
+				text = text.Replace("ﬁ", "fi")
+						.Replace("ﬂ", "fl")
+						.Replace("ﬀ", "ff")
+						.Replace("ﬃ", "ffi")
+						.Replace("’", "'");
+				File.WriteAllText(textPath, text);
+			}
+		}
+	}
+}
---
+++ Pdf2TextHelper/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Pdf2TextForAntConc")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pdf2TextForAntConc")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3af803ce-ae88-47b7-8fb6-49c07b4640f0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
